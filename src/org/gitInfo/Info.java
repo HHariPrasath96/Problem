c45f7f354aad9277f057fa89f80f5e6c61ebaa8a
HHariprasath96 <hariprasathalagesan@gmail.com>
2022-11-06 20:02:16 +0530

    Case1

---
+++ src/org/gitInfo/Info.java
@@ -0,0 +1,41 @@
+package org.gitInfo;
+
+public class Info {
+	
+	private void method() {
+        System.out.println("java");
+	}
+	
+	
+	private void method2() {
+    System.out.println("Pithon");
+    
+	}
+	
+	
+	private void method3() {
+      System.out.println("C#");
+	}
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
